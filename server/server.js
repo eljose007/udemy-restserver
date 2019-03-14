a26deb706363f82e9ae5e23adba29d1e2e5a9bfc
Jose Eduardo Gonzalez <jogonzalez@mides.interno>
2019-03-14 15:44:19 -0300

    cambios google sign in

--- server/server.js
+++ server/server.js
@@ -1,25 +1,34 @@
-require('./config/config')
+require("./config/config");
 
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 
-const app = express()
-const bodyParser = require('body-parser')
+const app = express();
+const bodyParser = require("body-parser");
 
+// habilitar la carpeta public
+
+app.use(express.static(path.resolve(__dirname, "../public")));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use( require('./routes/index'))
+app.use(require("./routes/index"));
 
-mongoose.connect(process.env.URLDB,{ useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
+mongoose.connect(
+  process.env.URLDB,
+  { useNewUrlParser: true, useCreateIndex: true },
+  (err, res) => {
     if (err) throw err;
 
-    console.log("base de datos conectada")
-});
+    console.log("base de datos conectada");
+  }
+);
 
-app.listen(process.env.PORT, 
-    () => console.log("Escuchando en puerto ", process.env.PORT))
+app.listen(process.env.PORT, () =>
+  console.log("Escuchando en puerto ", process.env.PORT)
+);
